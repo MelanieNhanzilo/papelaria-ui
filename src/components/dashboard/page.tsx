206e6fa39d1d7d00d9f8eed99e4c785d0f6accf4
MelanieNhanzilo <milenanhanzilo@gmail.com>
2024-11-14 13:00:52 +0200

    Mensagem descritiva das alterações

--- src/components/dashboard/page.tsx
+++ src/components/dashboard/page.tsx
@@ -13,13 +13,6 @@ import {
 
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-// import {
-//   Card,
-//   CardContent,
-//   CardDescription,
-//   CardHeader,
-//   CardTitle,
-// } from "@/components/ui/card";
 import {
   DropdownMenu,
   DropdownMenuContent,
