26fd9121b7a01011a4930b040ce18e0ddd605358
vitiliosousa <vitiliomartins2003@gmail.com>
2024-11-14 12:56:27 +0200

    layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link"
+
 export default function Home(){
   return(
-    <div>
-      <h1>Dashboard</h1>
+    <div className="flex items-center justify-center h-screen w-full flex-col gap-5">
+      <h1>Tela de login</h1>
+      <Link href={"/dashboard"} className="text-blue-500 underline">Dashboard</Link>
     </div>
   )
 }
